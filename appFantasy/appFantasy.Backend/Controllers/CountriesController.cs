a23c6bec885e2010ffd192ce751b4b959b6ece59
Carsoft <comando--12@hotmail.com>
2025-01-08 23:07:43 -0500

    Add countries API controller

---
+++ appFantasy/appFantasy.Backend/Controllers/CountriesController.cs
@@ -0,0 +1,74 @@
+﻿using appFantasy.Backend.Data;
+using appFantasy.Shared.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace appFantasy.Backend.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class CountriesController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public CountriesController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> PostAsync(Country country)
+        {
+            _context.Add(country);
+            await _context.SaveChangesAsync();
+            return Ok();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAsync()
+        {
+            var countries = await _context.Countries.ToListAsync();
+            return Ok(countries);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            return Ok();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> PutAsync(Country country)
+        {
+            var currentCountry = await _context.Countries.FindAsync(country.Id);
+            if (currentCountry == null)
+            {
+                return NotFound();
+            }
+            country.Name = currentCountry.Name;
+            _context.Update(currentCountry);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FindAsync(id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
+        }
+    }
+}
